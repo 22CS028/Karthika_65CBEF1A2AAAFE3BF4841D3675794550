9e05dc49e3dc0401f46523fba612a587e0e5ce5e
22CS028 <144211279+22CS028@users.noreply.github.com>
2023-09-14 15:50:21 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,20 +1,7 @@
-#
-class Player:
-    def play(self):
-        print("The player is playing cricket")
+#Write a program that determines whether a year entered by the user is a leap year or not using ifelif-else statements
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting")
-
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling")
-
-# Create objects of Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
-
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+year = 2023
+if (year % 4 == 0):
+  print("Leap year")
+else:
+  print("Not leap year")
